62b093b0264c57ab6bffcc70caef42ae56cab92b
Pierre-jean DUHAMEL <pduhamel@made-f0Ar7s8.clusters.42paris.fr>
2024-08-27 15:53:38 +0200

    db works, migrations won't at startup

--- backend/backend/settings/settings_base.py
+++ backend/backend/settings/settings_base.py
@@ -75,14 +75,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'postgres': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': '5432',
     }
