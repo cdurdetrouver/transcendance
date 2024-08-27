79cd8546ec3126a65e4550282c8e6aab8e9a5f91
Pierre-jean DUHAMEL <pduhamel@made-f0Dr11s10.clusters.42paris.fr>
2024-08-27 11:47:48 +0200

    change settings_base.py to add posgres in the django db settings

--- backend/backend/settings/settings_base.py
+++ backend/backend/settings/settings_base.py
@@ -77,6 +77,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
