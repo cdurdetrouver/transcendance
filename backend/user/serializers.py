b9d79faa539ab835030f26fa4ad11ce9b2c1efc2
cdurdetrouver <gabriel.bazart@gmail.com>
2024-09-03 00:37:45 +0200

    fix: hide password from everyone

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -4,5 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
-		fields = ['id', 'username', 'email', 'password', 'created_at', 'updated_at']
-
+		exclude = ['password']
