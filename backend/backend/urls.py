5eea7e67ec439862947820cbc4852cc1ce3ca0fb
cdurdetrouver <gabriel.bazart@gmail.com>
2024-09-06 13:37:15 +0200

    refactor: space in tab

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,23 +22,23 @@ from rest_framework import permissions
 from user.views import login, register
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="API Documentation",
-        default_version='v1',
-        description="Documentation for the API",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@school.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
+	openapi.Info(
+		title="API Documentation",
+		default_version='v1',
+		description="Documentation for the API",
+		terms_of_service="https://www.google.com/policies/terms/",
+		contact=openapi.Contact(email="contact@school.local"),
+		license=openapi.License(name="BSD License"),
+	),
+	public=True,
+	permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
-    path('login/', login),
-    path('register/', register),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('schema/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+	path('admin/', admin.site.urls),
+	path('user/', include('user.urls')),
+	path('login/', login),
+	path('register/', register),
+	path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+	path('schema/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
