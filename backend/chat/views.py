c26d37f8fb0a9bfd7e0e51d6c86514a598c3f0c0
Pierre-jean DUHAMEL <pduhamel@made-f0Ar5s3.clusters.42paris.fr>
2024-09-12 17:35:37 +0200

    test: creating base of a new chat with authentication

---
+++ backend/chat/views.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
+from twisted.application.runner.test.test_pidfile import ifPlatformSupported
+from user.views import 
+from autobahn.wamp import request
+import json
+
+@swagger_auto_schema(
+	method='get',
+	request_body=None,
+	responses={
+		200:"ok"
+	},
+	manual_parameters=[
+		openapi.Parameter(
+			'Authorization',
+			openapi.IN_HEADER,
+			description="Authorization token",
+			type=openapi.TYPE_STRING,
+			default='Bearer <token>'
+		)
+	],
+	operation_description="Retrieve a list of chat for user"
+)
+
+@api_view(['GET', 'POST'])
+def get_user_chats(request):
+    if request.method == 'GET':
+        user = request.user
+        print(user)
+
+@api_view()
+def websocket_connect(request):
+    # Vérifiez si la requête est bien une connexion WebSocket
+    if not request.is_web_socket():
+        return HttpResponse(status=400)
+
+    # Récupérez l'access token du payload
+    try:
+        data = json.loads(request.body)
+        access_token = data['accessToken']
+    except (KeyError, json.JSONDecodeError):
+        return HttpResponse(status=400)
