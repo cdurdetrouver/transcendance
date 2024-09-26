9a76b778c744a2a4c761688dd2c8ee6d55854e6b
Gabriel Bazart <gbazart@bess-f2r1s15.clusters.42paris.fr>
2024-09-26 12:37:48 +0200

    feat new fonction for clear curretn alert

--- frontend/src/components/alert/script.js
+++ frontend/src/components/alert/script.js
@@ -1,4 +1,5 @@
 let display = false;
+let timeoutid = null;
 
 function customalert(title, description, error = false) {
 	if (display) {
@@ -17,11 +18,22 @@ function customalert(title, description, error = false) {
 	alertDiv.getElementsByClassName('alert__title')[0].innerText = title;
 	alertDiv.getElementsByClassName('alert__description')[0].innerText = description;
 
-	setTimeout(() => {
+	timeoutid = setTimeout(() => {
 		alertDiv.classList.remove('error');
 		alertDiv.classList.remove('success');
 		display = false;
 	}, 4000);
 }
 
-export { customalert };
+function clearalert() {
+	if (display == false)
+		return (false);
+	clearTimeout(timeoutid);
+	const alertDiv = document.getElementsByClassName('alert')[0];
+	alertDiv.classList.remove('error');
+	alertDiv.classList.remove('success');
+	display = false;
+	return (true);
+}
+
+export { customalert, clearalert };
