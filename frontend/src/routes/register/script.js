15684be0c238ad732d445b6d97a8fe9d1297a716
Gabriel Bazart <gbazart@bess-f2r1s15.clusters.42paris.fr>
2024-09-26 12:38:54 +0200

    feat: add return in url param and it will redirect after the connexion

--- frontend/src/routes/register/script.js
+++ frontend/src/routes/register/script.js
@@ -1,11 +1,21 @@
+import { router } from "../../app.js";
 import { register } from "../../components/user/script.js";
 
-const form = document.querySelector('.signup form');
-form.addEventListener('submit', register_form);
+export async function initComponent() {
+	const form = document.querySelector('.signup form');
+	form.addEventListener('submit', register_form);
+}
 
-async function register_form(event)
-{
+export async function cleanupComponent() {
+	const form = document.querySelector('.signup form');
+	form.removeEventListener('submit', register_form);
+}
+
+async function register_form(event) {
 	event.preventDefault();
+	const urlParams = new URLSearchParams(window.location.search);
+	const return_path = urlParams.get('return');
+	const href = return_path ?? '/';
 	const username = document.querySelector('input[name="txt"]').value;
 	const email = document.querySelector('input[name="email"]').value;
 	const password = document.querySelector('input[name="pswd"]').value;
@@ -13,7 +23,7 @@ async function register_form(event)
 	const response = await register(username, email, password);
 
 	if (response.status === 201)
-		window.location.href = '/';
+		router.navigate(href);
 	else
 		alert('Registration failed');
 }
