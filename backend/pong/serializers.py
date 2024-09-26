926237240828458148587387fcc447b9feabff3b
Gabriel Bazart <gbazart@paul-f4Br3s3.clusters.42paris.fr>
2024-09-26 22:22:32 +0200

    fix: better game model for less request to the back

--- backend/pong/serializers.py
+++ backend/pong/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from .models import Game
 from drf_yasg import openapi
+from user.serializers import UserSerializer
 
 class GameSerializer(serializers.ModelSerializer):
-	game_swagger=openapi.Schema(
+	player1 = UserSerializer(read_only=True)
+	player2 = UserSerializer(read_only=True)
+	winner = UserSerializer(read_only=True)
+
+	game_swagger = openapi.Schema(
 		type=openapi.TYPE_OBJECT,
 		properties={
 			'id': openapi.Schema(type=openapi.TYPE_INTEGER),
@@ -12,11 +17,12 @@ class GameSerializer(serializers.ModelSerializer):
 			'started': openapi.Schema(type=openapi.TYPE_BOOLEAN),
 			'nb_players': openapi.Schema(type=openapi.TYPE_INTEGER),
 			'nb_viewers': openapi.Schema(type=openapi.TYPE_INTEGER),
-			'player1_id': openapi.Schema(type=openapi.TYPE_INTEGER),
-			'player2_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+			'player1': UserSerializer.user_swagger,
+			'player2': UserSerializer.user_swagger,
+			'winner': UserSerializer.user_swagger,
 			'winner_id': openapi.Schema(type=openapi.TYPE_INTEGER),
 			'player1_score': openapi.Schema(type=openapi.TYPE_INTEGER),
-			'player1_score': openapi.Schema(type=openapi.TYPE_INTEGER),
+			'player2_score': openapi.Schema(type=openapi.TYPE_INTEGER),
 			'created_at': openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_DATETIME),
 			'updated_at': openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_DATETIME),
 		}
@@ -24,4 +30,8 @@ class GameSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Game
-		fields = ['id', 'room_name', 'finished', 'started', 'nb_players', 'nb_viewers', 'player1_id', 'player2_id', 'winner_id', 'player1_score','player2_score', 'created_at', 'updated_at']
+		fields = [
+			'id', 'room_name', 'finished', 'started', 'nb_players', 'nb_viewers',
+			'player1', 'player2', 'winner', 'player1_score', 'player2_score',
+			'created_at', 'updated_at'
+		]
