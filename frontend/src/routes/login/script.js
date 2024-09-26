7ae51c16a8d150185c3ef55037f5873ff27b1874
Gabriel Bazart <gbazart@bess-f2r1s15.clusters.42paris.fr>
2024-09-26 12:38:54 +0200

    feat: add return in url param and it will redirect after the connexion

--- frontend/src/routes/login/script.js
+++ frontend/src/routes/login/script.js
@@ -19,15 +19,28 @@ export async function initComponent() {
 		logoutdiv.style.display = 'none';
 }
 
+export async function cleanupComponent() {
+	const logindiv = document.querySelector('.login');
+	const loginform = logindiv.querySelector('form');
+	loginform.removeEventListener('submit', login_form);
+
+	const logoutdiv = document.querySelector('.logout');
+	const logoutform = logoutdiv.querySelector('form');
+	logoutform.removeEventListener('submit', logout_form);
+}
+
 async function login_form(event) {
 	event.preventDefault();
+	const urlParams = new URLSearchParams(window.location.search);
+	const return_path = urlParams.get('return');
+	const href = return_path ?? '/';
 	const email = document.querySelector('input[name="email"]').value;
 	const password = document.querySelector('input[name="pswd"]').value;
 
 	const response = await login(email, password);
 
 	if (response.status === 200) {
-		router.navigate('/');
+		router.navigate(href);
 		customalert('Login successful', 'You are now logged in');
 	}
 	else {
