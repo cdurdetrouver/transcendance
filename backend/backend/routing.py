15a60a75e2912636253acb713ad0d5b911b31479
Gabriel Bazart <gbazart@bess-f2r1s15.clusters.42paris.fr>
2024-10-03 17:27:50 +0200

    fix: create new consumer

--- backend/backend/routing.py
+++ backend/backend/routing.py
@@ -1,9 +1,10 @@
 from django.urls import re_path
-from pong.consumers import PongConsumer, MatchmakingConsumer
+from pong.consumers import PongConsumer, MatchmakingConsumer, PrivateMatchmakingConsumer
 from chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
 	re_path("ws/chat/", ChatConsumer.as_asgi()),
 	re_path("ws/pong/matchmaking/", MatchmakingConsumer.as_asgi()),
+    re_path(r"ws/pong/privatematchmaking/(?P<room_name>[\w.!]+)/$", PrivateMatchmakingConsumer.as_asgi()),
 	re_path(r'ws/pong/(?P<room_name>[\w.!]+)/$', PongConsumer.as_asgi()),
 ]
