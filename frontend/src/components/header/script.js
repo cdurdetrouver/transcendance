68121f2ae8f770d1db9147d0bd87ac22489f479c
Ulysse CLEMENT <uclement@paul-f4Br5s5.clusters.42paris.fr>
2024-10-17 14:19:48 +0200

    add: fin

--- frontend/src/components/header/script.js
+++ frontend/src/components/header/script.js
@@ -13,10 +13,6 @@ setTimeout(function() {
 }, 100);
 
 
-
-
-
-
 export async function initComponent() {
 	const user = await get_user();
 
@@ -29,36 +25,19 @@ export async function initComponent() {
 		const profile_picture = user.picture_remote ? user.picture_remote : config.backendUrl + user.profile_picture;
 		imgElement.src = profile_picture;
 	}
-	else
-	{
-		const secondLi = document.querySelector('.bottom li:nth-child(2)');
-		const thirdLi = document.querySelector('.bottom li:nth-child(3)');
-		secondLi.remove();
-		
-		const imgElement = thirdLi.querySelector('.icon img'); // Select the img inside the .icon class
-
-		imgElement.src = '../../static/assets/header/login.png';
-		imgElement.alt = 'login-icon';
-		const textHeader = thirdLi.querySelector('.text-header');
-		textHeader.innerText = 'Login'; 
-	}
-
-
-
 
 	const sidebar = document.querySelector('.sidebar');
 	const iconImage = document.getElementById('iconImage');
 
 	sidebar.addEventListener('mouseenter', () => {
-		iconImage.src = '../../static/assets/header/head_cry_4.gif' ; // Change to your new icon source
+		iconImage.src = '../../static/assets/header/head_cry_4.gif' ;
 	});
 
 	sidebar.addEventListener('mouseleave', () => {
-		iconImage.src = '../../static/assets/jpg/head.png'; // Change back to the original icon
+		iconImage.src = '../../static/assets/jpg/head.png';
 	});
 
 }
-
 
 export async function cleanupComponent() {
 
