dfd679c16adccdf022e8c301aa6bc023a801577f
Ulysse CLEMENT <uclement@paul-f4Ar6s8.clusters.42paris.fr>
2024-10-22 17:07:53 +0200

    add: login linked to header + login refacto

--- frontend/src/routes/script.js
+++ frontend/src/routes/script.js
@@ -1,17 +1,17 @@
 
-import { getCookie, setCookie } from '../../components/storage/script.js';
+import { getCookie } from '../../components/storage/script.js';
 import { get_user } from '../../components/user/script.js';
-import { login, register, logout, login_tierce } from '../components/user/script.js';
+import { login, register, logout } from '../components/user/script.js';
 import {customalert} from '../components/alert/script.js';
 import config from '../env/config.js';
-import { router } from '../app.js';
+// import { router } from '../app.js';
 
 const ButtonGoogle = document.querySelector('.google_button');
 ButtonGoogle.href = `https://accounts.google.com/o/oauth2/auth?client_id=${config.google_id}&redirect_uri=${encodeURIComponent(config.frontendUrl + '/login?source=google')}&response_type=code&scope=openid%20email%20profile`;
 const ButtonGithub = document.querySelector('.github_button');
 ButtonGithub.href = `https://github.com/login/oauth/authorize?client_id=${config.github_id}&redirect_uri=${encodeURIComponent(config.frontendUrl + '/login?source=github')}&scope=user`;
 const ButtonIntra = document.querySelector('.intra_button');
-	ButtonIntra.href = `https://api.intra.42.fr/oauth/authorize?client_id=${config.intra_client_id}&redirect_uri=${encodeURIComponent(config.frontendUrl + '/login')}&response_type=code`;
+ButtonIntra.href = `https://api.intra.42.fr/oauth/authorize?client_id=${config.intra_client_id}&redirect_uri=${encodeURIComponent(config.frontendUrl + '/login')}&response_type=code`;
 
 const loginPopin = document.getElementById("login-popin");
 const logoutPopin = document.getElementById("logout-popin");
@@ -51,7 +51,7 @@ window.addEventListener("click", function(event) {
     }
 });
 
-const popin = document.getElementById("popin");
+const popin = document.querySelector(".popin");
 const loginForm = document.getElementById("login-content");
 
 const registerForm = document.getElementById("register-content");
