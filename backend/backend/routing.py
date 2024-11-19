06fa5d7a0e3e66358366b8f0e0fd0c41271b90f5
cdurdetrouver <gabriel.bazart@gmail.com>
2024-11-20 00:36:39 +0100

    feat: setup flappy game

--- backend/backend/routing.py
+++ backend/backend/routing.py
@@ -1,5 +1,6 @@
 from django.urls import re_path
 from pong.consumers import PongConsumer, MatchmakingConsumer, PrivateMatchmakingConsumer
+from flappy.consumers import FlappyConsumer, FlappyMatchmakingConsumer, FlappyPrivate
 from chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
@@ -7,4 +8,7 @@ websocket_urlpatterns = [
 	re_path("ws/pong/matchmaking/", MatchmakingConsumer.as_asgi()),
 	re_path(r"ws/pong/privatematchmaking/(?P<room_name>[\w.!]+)/$", PrivateMatchmakingConsumer.as_asgi()),
 	re_path(r'ws/pong/(?P<room_name>[\w.!]+)/$', PongConsumer.as_asgi()),
+	re_path("ws/flappy/matchmaking/", FlappyMatchmakingConsumer.as_asgi()),
+	# re_path(r"ws/flappy/privatematchmaking/(?P<room_name>[\w.!]+)/$", FlappyPrivateMatchmaking.as_asgi()),
+	re_path(r'ws/flappy/(?P<room_name>[\w.!]+)/$', FlappyConsumer.as_asgi()),
 ]
--- backend/flappy/views.py
+++ backend/flappy/views.py
@@ -1,3 +1,79 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.hashers import check_password, make_password
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.http import JsonResponse
+from django.conf import settings
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from .models import FlappyGame
+from .serializers import FlappyGameSerializer
+
+@swagger_auto_schema(
+	method='get',
+	request_body=None,
+	responses={
+		200: openapi.Schema(
+			type=openapi.TYPE_OBJECT,
+			properties={
+					'games': openapi.Schema(
+					type=openapi.TYPE_ARRAY,
+					items=FlappyGameSerializer.game_swagger
+				)
+			}
+		),
+		400: openapi.Schema(
+			type=openapi.TYPE_OBJECT,
+			properties={
+				'error': openapi.Schema(type=openapi.TYPE_STRING, description="Invalid credentials or validation error")
+			}
+		)
+	},
+	operation_description="Retrieve a list of games where the winner is not yet determined"
+)
+@api_view(['GET'])
+def game_detail(request):
+
+	games = FlappyGame.objects.filter(finished=False, started=True)
+	serialized_games = FlappyGameSerializer(games, many=True)
+
+	return JsonResponse({'games': serialized_games.data}, status=status.HTTP_200_OK)
+
+
+@swagger_auto_schema(
+	method='get',
+	request_body=None,
+	responses={
+		200: openapi.Schema(
+			type=openapi.TYPE_OBJECT,
+			properties={
+				'game':FlappyGameSerializer.game_swagger
+			}
+		),
+		404: openapi.Schema(
+			type=openapi.TYPE_OBJECT,
+			properties={
+				'error': openapi.Schema(type=openapi.TYPE_STRING, description="Game doesn't exist")
+			}
+		),
+		400: openapi.Schema(
+			type=openapi.TYPE_OBJECT,
+			properties={
+				'error': openapi.Schema(type=openapi.TYPE_STRING, description="Invalid credentials or validation error")
+			}
+		)
+	},
+	operation_description="Retrieve a game by id"
+)
+@api_view(['GET'])
+def game_id(request, game_id):
+	game = FlappyGame.objects.filter(id=game_id).first()
+	if game is None:
+		return Response({"error": "Game doesn't exist"}, status=status.HTTP_404_NOT_FOUND)
+	serialized_game = FlappyGameSerializer(game)
+	return JsonResponse({'game': serialized_game.data}, status=status.HTTP_200_OK)
