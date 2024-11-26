bfff2a27b73393149b3026e95572707ae220daee
Ulysse CLEMENT <uclement@made-f0Br3s1.clusters.42paris.fr>
2024-11-26 12:02:26 +0100

    add : front tournament player selection

--- frontend/src/routes/pong/tournament/script.js
+++ frontend/src/routes/pong/tournament/script.js
@@ -9,31 +9,52 @@ export async function initComponent() {
     const startButton = document.getElementById('start');
     const players = [];
   
-    addPlayerButton.addEventListener('click', () => {
-      const playerName = playerInput.value.trim();
-      if (playerName) {
-        players.push(playerName);
-        updatePlayerList();
-        playerInput.value = '';
-      }
-    });
+	addPlayerButton.addEventListener('click', addPlayer);
+
+	playerInput.addEventListener('keydown', (event) => {
+	  if (event.key === 'Enter') {
+		addPlayer();
+	  }
+	});
+
+	function addPlayer() {
+		const playerName = playerInput.value.trim();
+		if (playerName) {
+		  players.push(playerName);
+		  updatePlayerList();
+		  playerInput.value = '';
+		}
+	  }
   
     startButton.addEventListener('click', () => {
       if (players.length < 2) {
-        alert('Please add at least two players to start the tournament.');
+		customalert("Error", "Please add at least two players to start the tournament", true)
       } else {
         startTournament();
       }
     });
   
-    function updatePlayerList() {
+    function updatePlayerList(index) {
       playersDiv.innerHTML = '';
-      players.forEach((player, index) => {
+      players.forEach((player) => {
         const playerElement = document.createElement('div');
-        playerElement.textContent = `${index + 1}. ${player}`;
+		playerElement.classList.add('newPlayer');
+        playerElement.textContent = `${player}`;
+
+		const removeButton = document.createElement('button');
+		removeButton.id = 'removePlayer';
+		removeButton.addEventListener('click', () => removePlayer(index));
+
+		playerElement.appendChild(removeButton);
         playersDiv.appendChild(playerElement);
       });
+	  playersDiv.scrollTop = playersDiv.scrollHeight;
     }
+
+	function removePlayer(index) {
+		players.splice(index, 1);
+		updatePlayerList();
+	}
   
     function startTournament() {
         customalert('Tournament started', 'Good luck!', false);
