ed6acd7df6c9c77ca1a3ded05e36d8dc959a6c50
Ulysse CLEMENT <uclement@bess-f2r3s16.clusters.42paris.fr>
2024-11-27 18:31:34 +0100

    add: mon fetch tout pourri

--- frontend/src/routes/friends/script.js
+++ frontend/src/routes/friends/script.js
@@ -1,15 +1,34 @@
-import { get_user, searchUsers } from "../../components/user/script.js";
+import { get_user } from "../../components/user/script.js";
 import { router } from '../../app.js';
+import config from '../../env/config.js';
 import {customalert} from '../../components/alert/script.js';
 
+async function get_friend(user_id) {
+	const response = await fetch(config.backendUrl + '/user/friend/' + user_id, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		credentials: 'include'
+	});
+	if (response.status !== 200) {
+		console.error('Error connecting to friend');
+		customalert('Error', 'Error friend list', true);
+		return null;
+	}
+	// const friends = await response.json();
+	// console.log(friends);
+	// return friends;
+}
 
 
-export async function initComponent(params) {
-    user = await get_user();
+
+export async function initComponent(params) {	
+    let user = await get_user();
     if (!user)
         router.navigate('/login');
-	console.log(user);
-	console.log(user.friends);
+	console.log()
+	get_friend(user.id);
 }
 
 export async function cleanupComponent(params) {
