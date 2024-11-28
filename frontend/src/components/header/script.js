d51f5f6963ee6f8e3d76f44b926d384383b83d95
Ulysse Clément <51092179+Ulysseclem@users.noreply.github.com>
2024-11-28 14:56:51 +0100

    add: historic to user (#93)
    Co-authored-by: Ulysse CLEMENT <uclement@bess-f2r5s12.clusters.42paris.fr>

--- frontend/src/components/header/script.js
+++ frontend/src/components/header/script.js
@@ -1,10 +1,3 @@
-import { get_user} from '../../components/user/script.js';
-import { login, register } from '../user/script.js';
-import {customalert} from '../alert/script.js';
-import config from '../../env/config.js';
-
-
-
 setTimeout(function() {
     let headerElement = document.querySelector('header'); 
     
