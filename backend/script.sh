68861423177522285469c1d2137a3ed089661dfa
cdurdetrouver <gabriel.bazart@gmail.com>
2024-11-28 10:49:32 +0100

    Feature/delete account (#92)
    * feat: start cron jobs
    
    * test: test for cron
    
    * feat: cron jobs works
    
    * feat: script for check user inactivity
    
    ---------
    
    Co-authored-by: Gabriel Bazart <gbazart@bess-f2r3s17.clusters.42paris.fr>

--- backend/script.sh
+++ backend/script.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-# Check if a port parameter is provided
-if [ -z "$1" ]; then
-  echo "No port provided. Using default port 8000."
-  PORT=8000
-else
-  PORT=$1
+echo "Running Django setup tasks..."
+
+# Run migrations
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+
+# Check if the arguments are for running cron
+if [ "$1" = "cron" ] && [ "$2" = "-f" ]; then
+    echo "Adding crontab jobs..."
+    touch /var/log/cron.log
+    printenv | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID|LANG|PWD|GPG_KEY|_=' >> /etc/environment
+
+    python manage.py crontab remove
+    python3 manage.py crontab add
+    echo "Running cron service..."
 fi
 
-# Check if a settings path parameter is provided
-if [ -z "$2" ]; then
-  echo "No settings path provided. Using default settings."
-  SETTINGS_OPTION=""
-else
-  SETTINGS_OPTION="--settings=$2"
-fi
-
-# Run the Django management commands
-python3 manage.py makemigrations $SETTINGS_OPTION
-python3 manage.py migrate $SETTINGS_OPTION
-python3 manage.py runserver 0.0.0.0:$PORT $SETTINGS_OPTION
+# Execute the passed command
+exec "$@"
