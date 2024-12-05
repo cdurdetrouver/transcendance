7587679f4a233062acf2e1ff147e43cf37f20ec9
Ulysse Clément <51092179+Ulysseclem@users.noreply.github.com>
2024-12-05 14:06:09 +0100

    fix : number (#152)
    Co-authored-by: Ulysse CLEMENT <uclement@made-f0Ar1s3.clusters.42paris.fr>

--- frontend/src/routes/pong/game/script.js
+++ frontend/src/routes/pong/game/script.js
@@ -250,10 +250,10 @@ function draw(interpolatedState) {
 
     ctx.drawImage(
         headPlayer1,
-        5,
-        interpolatedState.paddle1Y,
-        56,
-		characterImages[player1Name].right.height
+        -2,
+        interpolatedState.paddle1Y - 6,
+        70,
+		67
     );
 
     // Paddle 2 logic
@@ -280,10 +280,10 @@ function draw(interpolatedState) {
 
     ctx.drawImage(
         headPlayer2,
-        canvas.width - paddleWidth - 10,
-        interpolatedState.paddle2Y,
-		56,
-		characterImages[player2Name].right.height
+        canvas.width - paddleWidth - 17,
+        interpolatedState.paddle2Y - 6,
+		70,
+		67
     );
 
     // Draw the ball
@@ -314,10 +314,10 @@ function draw_reset() {
     );
     ctx.drawImage(
         characterImages[player1Name].right,
-        5,
-        (canvas.height - paddleHeight) / 2,
-        characterImages[player1Name].right.width,
-        characterImages[player1Name].right.height
+        -2,
+        (canvas.height - paddleHeight) / 2 - 6,
+        70,
+        67
     );
 
     ctx.drawImage(
@@ -329,10 +329,10 @@ function draw_reset() {
     );
     ctx.drawImage(
         characterImages[player2Name].left,
-        canvas.width - paddleWidth - 10,
-        (canvas.height - paddleHeight) / 2,
-        characterImages[player2Name].left.width,
-        characterImages[player2Name].left.height
+        canvas.width - paddleWidth - 17,
+        (canvas.height - paddleHeight) / 2 - 6,
+        70,
+        67
     );
 
     ctx.drawImage(
